e9cb218bdd33d6c20e2b77bf4560eb1a14e6256d
sameer-ahmed-2000 <sameersameersmr20@gmail.com>
2024-06-11 14:43:19 +0530

    created user route

---
+++ backend/routes/user.js
@@ -0,0 +1,179 @@
+const express = require('express');
+const router = express.Router();
+const zod=require("zod");
+require('dotenv').config();
+const {User, Follower, Following, Post}=require("../db");
+const jwt= require("jsonwebtoken")
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const { authMiddleware } = require("../middleware")
+const signupSchema=zod.object({
+    username: zod.string().email(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    password: zod.string(),
+    
+})
+router.post("/signup",async (req,res)=>{
+    const {success}=signupSchema.safeParse(req.body);
+    if (!success){
+        return res.status(411).json({
+            message:"Incorrect inputs",error: error.errors
+        })
+    }
+    const existingUser =await User.findOne({
+        username:req.body.username
+    })
+    if (existingUser){
+        return res.status(411).json({
+            message:"Email is already taken"
+            
+        })
+    }
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+
+    res.json({
+        message:"User created successfully",
+        token: token
+    })
+})
+const signinBody=zod.object({
+    username:zod.string().email(),
+    password:zod.string()
+})
+router.post("/signin",async (req,res)=>{
+    const {success}=signinBody.safeParse(req.body)
+    if (!success){
+        return res.status(411).json({
+            message:"Incorrect inputs"
+        })
+    }
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+    if (user){
+        const token=jwt.sign({
+            userId:user._id
+        },JWT_SECRET);
+        res.json({
+            token:token
+        })
+        return;
+    }
+    res.status(411).json({
+        message:"Error while logging in"
+    })
+})
+
+router.get('/followercount',authMiddleware,async(req,res)=>{
+    try{
+        const user=await Follower.findById(req.userId);
+        const numberOfFollowers= user?user.followers.length:0;
+        res.json({numberOfFollowers});
+
+    }catch(error){
+        res.status(500).send(error.message);
+    }
+})
+
+router.get('/followingcount',authMiddleware,async(req,res)=>{
+    try{
+        const user=await Following.findById(req.userId);
+        const numberOfFollowing= user?user.following.length:0;
+        res.json({numberOfFollowing});
+
+    }catch(error){
+        res.status(500).send(error.message);
+    }
+})
+
+router.get('/postcount',authMiddleware,async(req,res)=>{
+    try{
+        const post=await Post.find({author:req.userId});
+        const numberOfPost= post?post.length:0;
+        res.json({numberOfPost});
+
+    }catch(error){
+        res.status(500).send(error.message);
+    }
+})
+router.get('/users', authMiddleware, async (req, res) => {
+    try {
+        // Fetch all users except the current user
+        const users = await User.find({ _id: { $ne: req.userId } });
+        // Fetch the current user's following list
+        const currentUserFollowing = await Following.findOne({ user: req.userId });
+    
+      // Map through all users to add following status and count
+        const usersWithFollowingInfo = await Promise.all(users.map(async (user) => {
+        const isFollowing = currentUserFollowing.following.includes(user._id);
+        // Count the number of following for each user
+        const followingCountDoc = await Following.findOne({ user: user._id });
+        const followingCount = followingCountDoc ? followingCountDoc.following.length : 0;
+        return {
+            ...user.toObject(),
+            isFollowing,
+            followingCount
+        };
+        }));
+    res.json(usersWithFollowingInfo);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.get('/followers', authMiddleware, async (req, res) => {
+    try {
+        const followersDoc = await Follower.findOne({ user: req.userId }).populate('followers');
+        const currentUserFollowingDoc = await Following.findOne({ user: req.userId });
+        const followersDetails = await Promise.all(followersDoc.followers.map(async (follower) => {
+            const isFollowing = currentUserFollowingDoc.following.includes(follower._id);
+            
+            const followingDoc = await Following.findOne({ user: follower._id });
+            const followingCount = followingDoc ? followingDoc.following.length : 0;
+            return {
+                _id: follower._id,
+                username: follower.username,
+                firstName: follower.firstName,
+                lastName: follower.lastName,
+                isFollowing,
+                followingCount
+        };
+        }));
+        res.json({ followersDetails });
+        } catch (error) {
+        res.status(500).send(error.message);
+        }
+});
+router.get('/following', authMiddleware, async (req, res) => {
+    try {
+        const currentUserFollowingDoc = await Following.findOne({ user: req.userId }).populate('following');
+        const followingDetails = await Promise.all(currentUserFollowingDoc.following.map(async (followedUser) => {
+            const followingCountDoc = await Following.findOne({ user: followedUser._id });
+            const followingCount = followingCountDoc ? followingCountDoc.following.length : 0;
+            return {
+                _id: followedUser._id,
+                username: followedUser.username,
+                firstName: followedUser.firstName,
+                lastName: followedUser.lastName,
+                followingCount
+                };
+        }));
+        res.json(followingDetails);
+        } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+router.get("/me",authMiddleware,async (req,res)=>{
+    const user= await User.findById(req.userId);
+    res.json({
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        password: user.password
+
+    })
+});
