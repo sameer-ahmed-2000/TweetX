1e157e0b1e1138a92214a9d8e4d7534cd14753eb
sameer-ahmed-2000 <sameersameersmr20@gmail.com>
2024-06-11 16:27:37 +0530

    updated feed route

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require('../middleware');
 router.post('/posts', authMiddleware, async (req, res) => {
     try {
         const { content } = req.body;
-        const authorId = req.user.id;
+        const authorId = req.userId;
         const newPost = new Post({ content, author: authorId });
         await newPost.save();
         res.status(201).json(newPost);
@@ -15,11 +15,21 @@ router.post('/posts', authMiddleware, async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
-router.get('/feed/:userId', async (req, res) => {
+router.get('/myposts', authMiddleware, async (req, res) => {
     try {
-        const { userId } = req.params;
-        const followingList = await Following.findOne({ user: userId }).populate('following');
-        const followingIds = followingList.following.map(user => user._id);
+        const myPosts = await Post.find({ author: req.userId })
+                                    .sort({ createdAt: -1 }) // Sort by newest first
+                                    .populate('author', 'username');
+        res.json(myPosts);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.get('/feed', authMiddleware, async (req, res) => {
+    try {
+        const followingList = await Following.findOne({ user: req.userId }).populate('following');
+        const followingIds = followingList ? followingList.following.map(user => user._id) : [];
 
         const posts = await Post.find({ author: { $in: followingIds } })
                                 .sort({ createdAt: -1 }) // Sort by newest first
@@ -29,4 +39,5 @@ router.get('/feed/:userId', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
 module.exports = router;
