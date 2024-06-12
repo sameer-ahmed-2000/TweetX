3d91d90a3d4084a7b710f859783a6374fd9e3e01
sameer-ahmed-2000 <sameersameersmr20@gmail.com>
2024-06-12 18:44:22 +0530

    created signup hook

---
+++ frontend/src/hooks/useSignup.jsx
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export function useSignup() {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [fullName, setFullname] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmpassword, setConfirmpassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    const handleSignup = async () => {
+        if (!username || !fullName || !password || !confirmpassword || password !== confirmpassword) {
+            setError('Please fill in all fields and make sure passwords match');
+            return;
+        }
+        setLoading(true);
+        setError('');
+        try {
+            const response = await axios.post('http://localhost:3001/api/v1/user/signup', {
+                username,
+                fullName,
+                password
+            });
+            localStorage.setItem('token', response.data.token);
+            navigate('/main');
+        } catch (err) {
+            setError('Sign up failed, Please check your credentials and try again');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return {
+        username,
+        setUsername,
+        fullName,
+        setFullname,
+        confirmpassword,
+        setConfirmpassword,
+        password,
+        setPassword,
+        loading,
+        error,
+        handleSignup
+    };
+}
