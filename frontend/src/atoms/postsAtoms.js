e2b7d9f2c97a9bef63a1b6ff8a1f01ff3c2aa78c
sameer-ahmed-2000 <sameersameersmr20@gmail.com>
2024-06-18 18:30:59 +0530

    created atom

---
+++ frontend/src/atoms/postsAtoms.js
@@ -0,0 +1,38 @@
+import { atom, selector } from 'recoil';
+import axiosInstance from '../functions/axiosInstance';
+
+export const userPostsState = atom({
+    key: 'userPostsState',
+    default: [],
+});
+
+export const otherPostsState = atom({
+    key: 'otherPostsState',
+    default: [],
+});
+
+export const fetchUserPostsSelector = selector({
+    key: 'fetchUserPostsSelector',
+    get: async () => {
+        try {
+        const response = await axiosInstance.get('feed/myposts');
+        return response.data;
+        } catch (error) {
+        console.error('Error fetching user posts:', error);
+        return [];
+        }
+    },
+});
+
+export const fetchOtherPostsSelector = selector({
+    key: 'fetchOtherPostsSelector',
+    get: async () => {
+        try {
+        const response = await axiosInstance.get('feed/feed');
+        return response.data;
+        } catch (error) {
+        console.error('Error fetching other posts:', error);
+        return [];
+        }
+    },
+});
